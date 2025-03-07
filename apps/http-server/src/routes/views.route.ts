af7eac4f484b4d243d466a2fdceadee4162516cd
Md Mahtab Ansari <m.a.raj58232@gmail.com>
2025-03-07 16:17:19 +0530

    feat: remove unused paramsValidator import from views route

--- apps/http-server/src/routes/views.route.ts
+++ apps/http-server/src/routes/views.route.ts
@@ -1,6 +1,5 @@
 import {postIdObj} from "@workspace/schema/views";
 import {bodyValidator} from "../validators/body.validator";
-import {paramsValidator} from "../validators/params.validator";
 import {viewsController} from "../controllers/views.controller";
 import {accessTokenValidator} from "../validators/jwt.validator";
 import {Router} from "express";
